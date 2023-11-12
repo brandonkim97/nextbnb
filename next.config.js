7b58ff21e530257749e515ae3407dbb0db5d935f
Brandon Kim <kimbrandon97@gmail.com>
2023-11-11 22:12:47 -0800

    Added secure headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
+const headers = require('/headers');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: [
             "avatars.githubusercontent.com",
             "res.cloudinary.com",
+        ]
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers,
+            }
         ]
     }
 }
